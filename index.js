4c976672660d4282ea4701384845475e55d6abeb
kellenmay <kellenbmay@gmail.com>
2021-06-10 20:58:30 -0400

    commit tree 9c1107aeba2f8bdba2344f1732130eb8bcd6b610

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 // Code your solution here
-function findMatching(source, sought) {
-    return source.filter( possibleMatch =>
-        possibleMatch.toLowerCase() === sought.toLowerCase()
-        )
+
+const { match } = require("sinon")
+
+function findMatching(drivers, string){
+    return drivers.filter( search => 
+        search.toLowerCase() === string.toLowerCase()
+    )
 }
 
-function fuzzyMatch(source, testString) {
-    return source.filter( possibleMatch =>
-        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+function fuzzyMatch(drivers, string) {
+    return drivers.filter( search => 
+        search.toLowerCase().indexOf(string.toLowerCase()) === 0
         )
+
+
 }
 
-function matchName(source, soughtName) {
-    return source.filter( record => record.name === soughtName )
-}
+
+
+
+
+
+// function fuzzyMatch(source, testString) {
+//     return source.filter( possibleMatch =>
+//         possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+//         )
+// }
+
+// function matchName(source, soughtName) {
+//     return source.filter( record => record.name === soughtName )
+// }
